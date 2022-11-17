e61fcf278a59f96761b7d9caf7b85adbdc974ac2
kelmerCM <kcarrillo@uniguajira.edu.co>
2022-11-17 18:55:20 -0500

    master

---
+++ src/app/components/tipoProducto/mostrar-tipo-producto/mostrar-tipo-producto.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-mostrar-tipo-producto',
+  templateUrl: './mostrar-tipo-producto.component.html',
+  styleUrls: ['./mostrar-tipo-producto.component.css']
+})
+export class MostrarTipoProductoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
